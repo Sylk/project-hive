b684aad60aa98f80415477728b22b1299bf6d43e
Sylk <sylk@protonmail.com>
2026-01-12 04:36:57 -0800

    Added illegal parts back in

--- Dolphin/User/Load/Riivolution/GXCE/Codes/Source/Parts/PartData/Custom/PartList.asm
+++ Dolphin/User/Load/Riivolution/GXCE/Codes/Source/Parts/PartData/Custom/PartList.asm
@@ -1,10 +1,43 @@
 #Custom Robo Battle Revolution Base Game Parts
 #Body
+.include "Source/Parts/PartData/Custom/Body/Athena.asm"
+.include "Source/Parts/PartData/Custom/Body/Oil Can.asm"
+.include "Source/Parts/PartData/Custom/Body/Rahu I.asm"
+.include "Source/Parts/PartData/Custom/Body/Rahu II.asm"
+.include "Source/Parts/PartData/Custom/Body/Rahu III.asm"
+.include "Source/Parts/PartData/Custom/Body/Rakansen.asm"
+.include "Source/Parts/PartData/Custom/Body/Ray Legend.asm"
+.include "Source/Parts/PartData/Custom/Body/Ray Warrior.asm"
+.include "Source/Parts/PartData/Custom/Body/Ruhiel.asm"
 
 #Guns
+.include "Source\Parts\PartData\Custom\Gun\Can Gun.asm"
+.include "Source\Parts\PartData\Custom\Gun\Crystal Strike Gun.asm"
+.include "Source\Parts\PartData\Custom\Gun\Rahu II Gun.asm"
+.include "Source\Parts\PartData\Custom\Gun\Rahu III Gun.asm"
+.include "Source\Parts\PartData\Custom\Gun\Raptor Gun.asm"
+.include "Source\Parts\PartData\Custom\Gun\SwitchMagnum.asm"
+.include "Source\Parts\PartData\Custom\Gun\Waning Arc Gun.asm"
+.include "Source\Parts\PartData\Custom\Gun\Wave Laser Gun.asm"
+.include "Source\Parts\PartData\Custom\Gun\Waxing Arc Gun.asm"
+.include "Source\Parts\PartData\Custom\Gun\Wyrm Gun.asm"
+.include "Source\Parts\PartData\Custom\Gun\X Laser Gun.asm"
 
 #Bomb
+.include "Source/Parts/PartData/Custom/Bomb/Can Bomb.asm"
+.include "Source/Parts/PartData/Custom/Bomb/Treble Bomb.asm"
+.include "Source/Parts/PartData/Custom/Bomb/Waning Arc Bomb.asm"
+.include "Source/Parts/PartData/Custom/Bomb/Waxing Arc Bomb.asm"
+.include "Source/Parts/PartData/Custom/Bomb/Wyvern Bomb.asm"
 
 #Pods
+.include "Source\Parts\PartData\Custom\Pod\Can Pod.asm"
+.include "Source\Parts\PartData\Custom\Pod\Cheetah Pod.asm"
+.include "Source\Parts\PartData\Custom\Pod\Orca Pod.asm"
+.include "Source\Parts\PartData\Custom\Pod\Penumbra I.asm"
+.include "Source\Parts\PartData\Custom\Pod\Penumbra II.asm"
+.include "Source\Parts\PartData\Custom\Pod\Penumbra III.asm"
+.include "Source\Parts\PartData\Custom\Pod\Wolf Spider Pod.asm"
 
 #Legs
+.include "Source/Parts/PartData/Custom/Leg/Can Legs.asm"
