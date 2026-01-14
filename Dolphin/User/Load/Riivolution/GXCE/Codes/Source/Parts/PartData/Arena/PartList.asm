4f60585ac966152c4f56e6e5aa313936ca34e014
Sylk <sylk@protonmail.com>
2026-01-14 02:10:15 -0800

    Organizes part data into Arena and Custom categories
    Moves part data files to better reflect their origin.
    
    Parts originally from the Arena game are now in the Arena directory.
    Parts that are custom are now in the Custom directory,
    removed all the parts that were not associated with the new directory out of the catch all old one
    renamed custom directory to BattleRevolutionIllegal to better reflect their origin.
    
    Creates a new PartList file for the Arena parts to keep
    the part data organized.

---
+++ Dolphin/User/Load/Riivolution/GXCE/Codes/Source/Parts/PartData/Arena/PartList.asm
@@ -0,0 +1,25 @@
+# Parts ported from Arena
+# Body
+
+.include "Source/Parts/PartData/Arena/Body/WSRayLegend.asm"
+.include "Source/Parts/PartData/Arena/Body/WSRakansen.asm"
+.include "Source/Parts/PartData/Arena/Body/WSRayWarrior.asm"
+
+.include "Source/Parts/PartData/Arena/Body/BBChickenheart.asm"
+.include "Source/Parts/PartData/Arena/Body/BBRahu2.asm"
+.include "Source/Parts/PartData/Arena/Body/BBRuhiel.asm"
+
+.include "Source/Parts/PartData/Arena/Body/LDSwift.asm"
+.include "Source/Parts/PartData/Arena/Body/LDDefender.asm"
+.include "Source/Parts/PartData/Arena/Body/LDBreaker.asm"
+
+.include "Source/Parts/PartData/Arena/Body/SSAthena.asm"
+.include "Source/Parts/PartData/Arena/Body/SSSeeker.asm"
+.include "Source/Parts/PartData/Arena/Body/SSMilkyWay.asm"
+
+# Gun
+.include "Source/Parts/PartData/Arena/Gun/Slide Gun.asm"
+
+# Legs
+.include "Source/Parts/PartData/Arena/Leg/Plus One Legs.asm"
+
